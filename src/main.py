9cb7557238c78eff12805cd10f9b481cd96e688b
Roy <cjhroy@outlook.com>
2024-11-13 18:15:10 +0800

    updated origins

--- src/main.py
+++ src/main.py
@@ -19,10 +19,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-    "https://viteform-five.vercel.app",
-    "http://localhost:5173"
-    ],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
