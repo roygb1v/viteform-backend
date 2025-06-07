f2ed6517497c344f794e6e9c7c0d324a1b763659
ROY CHEONG <cjhroy@outlook.com>
2025-06-08 04:07:38 +0800

    updated wildcard

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(function (req, res, next) {
   console.log({isProduction, env: process.env.NODE_ENV});
 
   // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', 'https://www.viteform.io');
+  res.setHeader('Access-Control-Allow-Origin', '*');
 
   // Request methods you wish to allow
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
